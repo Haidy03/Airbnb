fa3e6f48e7e54a5b87b2cc49a5876d429e12d5c7
Rahma Salah <aminroro75@gmail.com>
2025-11-16 16:36:38 +0200

    Package

--- Backend/Airbnb.API/obj/Debug/net9.0/Airbnb.API.AssemblyInfo.cs
+++ Backend/Airbnb.API/obj/Debug/net9.0/Airbnb.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Airbnb.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad7fe59cd283eda37f98a26ae3d46d84ca43d6e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7630c705e0864d3dec3b68cfe13d68d042f3f39f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Airbnb.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Airbnb.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
