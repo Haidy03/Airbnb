d8c1e34d834293a3f6c639585f8f0d28331f3abe
Haidy Mohamed <haidymohamed650@gmail.com>
2025-11-17 01:20:09 +0200

    host feature

--- Frontend/airbnb-frontend/src/app/app.routes.ts
+++ Frontend/airbnb-frontend/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { HostLayoutComponent } from './layouts/host-layout/host-layout';
 import { HostDashboardComponent } from './features/host/components/host-dashboard/host-dashboard';
+import { HostCalendar } from './features/host/components/host-calendar/host-calendar';
+import { MyProperties } from './features/host/components/my-properties/my-properties';
+import { HostMessages } from './features/host/components/host-messages/host-messages';
 
 export const routes: Routes = [
   {
@@ -9,6 +12,9 @@ export const routes: Routes = [
     children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'dashboard', component: HostDashboardComponent },
+      { path: 'calendar', component: HostCalendar },
+      { path: 'properties', component: MyProperties },
+      { path: 'messages', component: HostMessages },
       // Add more routes here as we build them
     ]
   },
