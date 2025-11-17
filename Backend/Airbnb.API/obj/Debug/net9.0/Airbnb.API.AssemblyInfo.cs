ce2c6abc3ae6e26f5d46178e8b4aa9abb445dcb9
Rahma Salah <aminroro75@gmail.com>
2025-11-17 15:59:21 +0200

    commit

--- Backend/Airbnb.API/obj/Debug/net9.0/Airbnb.API.AssemblyInfo.cs
+++ Backend/Airbnb.API/obj/Debug/net9.0/Airbnb.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Airbnb.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84135f7638268c3fbb7792c9ff32f4566bd38782")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcfff023f5b753d1b7489ab79d336b3e9be953e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Airbnb.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Airbnb.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
