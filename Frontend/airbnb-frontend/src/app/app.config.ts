acb9dd5b4f22f8659f11c5eb5b9557a392d54c10
Mossad <mossadhesham205@gmail.com>
2025-11-19 15:41:39 +0200

    Merge main into Mossad and resolve all frontend conflicts

--- Frontend/airbnb-frontend/src/app/app.config.ts
+++ Frontend/airbnb-frontend/src/app/app.config.ts
@@ -1,31 +1,24 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-<<<<<<< HEAD
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
 import { importProvidersFrom } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-=======
->>>>>>> 221cd89216df6657bd4a1a742d2898ca29234b85
 import { routes } from './app.routes';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-<<<<<<< HEAD
 
-importProvidersFrom(FormsModule),
+    provideAnimations(),
 
-    // --- HTTP Client ---
-    provideHttpClient(),
 
-    // --- Router Setup ---
+    importProvidersFrom(FormsModule),
+
+
+    provideHttpClient(withInterceptorsFromDi()),
+
     provideRouter(routes),
-=======
-    provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi())
->>>>>>> 221cd89216df6657bd4a1a742d2898ca29234b85
-  ]
+  ],
 };
