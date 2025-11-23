ae45b51d660177e1485247b02c24493a0586a7bc
Rahma Salah <aminroro75@gmail.com>
2025-11-24 00:40:09 +0200

    Messages Update

--- Frontend/airbnb-frontend/src/app/features/auth/services/auth.service.ts
+++ Frontend/airbnb-frontend/src/app/features/auth/services/auth.service.ts
@@ -87,11 +87,8 @@ private readonly ROLE_KEY = 'userRole';
   private getUserFromStorage(): AuthUser | null {
     const userId = localStorage.getItem(this.USER_ID_KEY);
     const email = localStorage.getItem(this.EMAIL_KEY);
-<<<<<<< HEAD
     const role = localStorage.getItem(this.ROLE_KEY);
-=======
 
->>>>>>> origin
     if (!userId) return null;
 
     return {
