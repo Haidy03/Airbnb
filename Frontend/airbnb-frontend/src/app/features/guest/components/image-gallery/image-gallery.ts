c767aa042abfe883d354111065a322d0f9d81fd2
sohilasalah16 <120594461+sohilasalah16@users.noreply.github.com>
2025-11-26 22:44:54 +0200

    update show photo

--- Frontend/airbnb-frontend/src/app/features/guest/components/image-gallery/image-gallery.ts
+++ Frontend/airbnb-frontend/src/app/features/guest/components/image-gallery/image-gallery.ts
@@ -1,6 +1,7 @@
 
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { environment } from '../../../../../environments/environment';
 
 
 
@@ -14,7 +15,16 @@ import { CommonModule } from '@angular/common';
 export class ImageGallery {
    // نستقبل الصور من الـ API عبر المكون الأب
   // تأكدي أن الـ API يرسل 5 صور على الأقل لهذا التصميم
-  @Input() images:any[] = [];
+  @Input() images:string[] = [];
+   title: string = 'Property Image';
+    imageBaseUrl: string = environment.imageBaseUrl;
+     getFullImageUrl(relativePath: string): string {
+    if (!relativePath) {
+      return 'assets/images/placeholder.jpg'; // صورة احتياطية
+    }
+    // دمج عنوان الباك إند الأساسي مع المسار النسبي
+    return `${this.imageBaseUrl}${relativePath}`;
+  }
 
   showAllPhotos() {
     console.log('Open full screen modal');
