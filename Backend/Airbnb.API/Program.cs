f9209a5d6dcf56f767f20518219c757b13c92dce
Mossad <mossadhesham205@gmail.com>
2025-11-27 03:06:58 +0200

    new port

--- Backend/Airbnb.API/Program.cs
+++ Backend/Airbnb.API/Program.cs
@@ -140,7 +140,7 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins(
                 "http://localhost:4200",
-                "http://localhost:52290"
+                "http://localhost:55953"
               )
               .AllowAnyMethod()
               .AllowAnyHeader()
