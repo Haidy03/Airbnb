5ed5b1fe3fabb8f60b6b75b7c3ab19bf1e0200b9
Karim Reda <karim.reda.69420@gmail.com>
2025-11-29 00:46:55 +0200

    done

--- Backend/Airbnb.API/DTOs/Auth/UpdateProfileDto.cs
+++ Backend/Airbnb.API/DTOs/Auth/UpdateProfileDto.cs
@@ -30,5 +30,19 @@ namespace Airbnb.API.DTOs.Auth
         public string? City { get; set; }
 
         public string? Country { get; set; }
+        public string? AboutMe { get; set; }
+        public string? WhereToGo { get; set; }
+        public string? MyWork { get; set; }
+        public string? SpendTime { get; set; }
+        public string? Pets { get; set; }
+        public string? BornDecade { get; set; }
+        public string? School { get; set; }
+        public string? UselessSkill { get; set; }
+        public string? FunFact { get; set; }
+        public string? FavoriteSong { get; set; }
+        public string? ObsessedWith { get; set; }
+        public string? BiographyTitle { get; set; }
+        public string? Languages { get; set; }
+        public string? WhereILive { get; set; }
     }
 }
