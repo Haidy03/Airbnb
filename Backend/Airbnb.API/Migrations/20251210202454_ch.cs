ae012f8fba77dd1850ea890e49a853579fe87488
Rahma Salah <aminroro75@gmail.com>
2025-12-10 22:28:47 +0200

    Edits

---
+++ Backend/Airbnb.API/Migrations/20251210202454_ch.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Airbnb.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class ch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Services_ServiceCategories_CategoryId",
+                table: "Services");
+
+            migrationBuilder.DeleteData(
+                table: "ServiceCategories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ServiceCategories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "ServiceCategories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "ServiceCategories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Services_ServiceCategories_CategoryId",
+                table: "Services",
+                column: "CategoryId",
+                principalTable: "ServiceCategories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Services_ServiceCategories_CategoryId",
+                table: "Services");
+
+            migrationBuilder.InsertData(
+                table: "ServiceCategories",
+                columns: new[] { "Id", "Description", "DisplayOrder", "Icon", "IsActive", "Name" },
+                values: new object[,]
+                {
+                    { 1, null, 1, "chef-hat", true, "Chefs" },
+                    { 2, null, 2, "dumbbell", true, "Training" },
+                    { 3, null, 3, "spa", true, "Beauty" },
+                    { 4, null, 4, "broom", true, "Cleaning" }
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Services_ServiceCategories_CategoryId",
+                table: "Services",
+                column: "CategoryId",
+                principalTable: "ServiceCategories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
